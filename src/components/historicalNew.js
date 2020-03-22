b9f3cecda8b506c87f611b88d7b0ff9aff7b6e8e
rituraj <riturajratan@gmail.com>
2020-03-22 17:21:39 +0530

    set width
    Signed-off-by: rituraj <riturajratan@gmail.com>

--- src/components/historicalNew.js
+++ src/components/historicalNew.js
@@ -33,8 +33,8 @@ export default function HistoricalNew() {
   const [viewport, setViewport] = useState({
     latitude: 21,
     longitude: 78,
-    width: "55vw",
-    height: "92vh",
+    width: "100%",
+    height: "100%",
     zoom: 3
   });
   const url = API.map;
