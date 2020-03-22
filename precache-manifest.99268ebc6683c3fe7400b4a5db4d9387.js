26e182124a959db06b0e48ea1bd5d82b27bac574
rituraj <riturajratan@gmail.com>
2020-03-22 15:55:06 +0530

    Initial dist subtree commit

---
+++ precache-manifest.99268ebc6683c3fe7400b4a5db4d9387.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "959eb714aa127b7f22463414bb3e52dc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2167728edc7453bb93aa",
+    "url": "/static/css/main.e669eb90.chunk.css"
+  },
+  {
+    "revision": "be61fc9fa768651e452c",
+    "url": "/static/js/2.a8a0e78c.chunk.js"
+  },
+  {
+    "revision": "1260a55b595e18a66880ee491376c98d",
+    "url": "/static/js/2.a8a0e78c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2167728edc7453bb93aa",
+    "url": "/static/js/main.5cc42bc8.chunk.js"
+  },
+  {
+    "revision": "09eb2fae383ea5eae762",
+    "url": "/static/js/runtime-main.074d3413.js"
+  },
+  {
+    "revision": "43055c391fa5c1d1be2ab2c29d9a8557",
+    "url": "/static/media/corona.43055c39.png"
+  }
+]);
