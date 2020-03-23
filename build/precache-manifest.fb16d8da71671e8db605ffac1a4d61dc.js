9c7908fb1c78619f97bcee2b16149d5d9d2fec3a
rituraj <riturajratan@gmail.com>
2020-03-23 09:28:05 +0530

    add new build

--- build/precache-manifest.fb16d8da71671e8db605ffac1a4d61dc.js
+++ build/precache-manifest.fb16d8da71671e8db605ffac1a4d61dc.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "f8d26313220b2f15f829ddfbea71d193",
+    "revision": "942768605762de9bafa2d76738e182de",
     "url": "/corona/index.html"
   },
   {
-    "revision": "3b7b6f20dfcbb4fd1ffc",
+    "revision": "5fae4513ce68ec3e6a2a",
     "url": "/corona/static/css/main.e669eb90.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/corona/static/js/2.3c36d88b.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "3b7b6f20dfcbb4fd1ffc",
-    "url": "/corona/static/js/main.8eb1156e.chunk.js"
+    "revision": "5fae4513ce68ec3e6a2a",
+    "url": "/corona/static/js/main.83fb812f.chunk.js"
   },
   {
     "revision": "a3b787bfbf2c81f2e0d4",
