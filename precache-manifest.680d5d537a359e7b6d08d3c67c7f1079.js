c54c8f2c44464fa74084c897b7c4c5ad4b9a0545
rituraj <riturajratan@gmail.com>
2020-04-25 00:31:27 +0530

    added country icon and test

---
+++ precache-manifest.680d5d537a359e7b6d08d3c67c7f1079.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef7437f86fa45d61536be4904bc36c77",
+    "url": "/corona/index.html"
+  },
+  {
+    "revision": "53226a1efa2c539c7b47",
+    "url": "/corona/static/css/main.e669eb90.chunk.css"
+  },
+  {
+    "revision": "c26703c9c8fc713f91be",
+    "url": "/corona/static/js/2.799e784e.chunk.js"
+  },
+  {
+    "revision": "1260a55b595e18a66880ee491376c98d",
+    "url": "/corona/static/js/2.799e784e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53226a1efa2c539c7b47",
+    "url": "/corona/static/js/main.31ff685a.chunk.js"
+  },
+  {
+    "revision": "a3b787bfbf2c81f2e0d4",
+    "url": "/corona/static/js/runtime-main.c6168e55.js"
+  },
+  {
+    "revision": "43055c391fa5c1d1be2ab2c29d9a8557",
+    "url": "/corona/static/media/corona.43055c39.png"
+  }
+]);
